925864c28e9e2a7decf5ccdd0211bf35adad48a0
MollyBreckler <lizabreckler@Liza-Brecklers-MacBook-Pro.local>
2017-05-18 13:33:13 +1000

    project 3 readings

---
+++ rollover/assets/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+  $('.oase').mouseenter(function() {
+    $('.oase__image').addClass('showme');
+  });
+
+
+    $('.oase').mouseleave(function() {
+      $('.oase__image').removeClass('showme');
+    });
+
+
+
+      $('.93').mouseenter(function() {
+        $('.93__image').addClass('showme');
+      });
+
+
+        $('.93').mouseleave(function() {
+          $('.93__image').removeClass('showme');
+        });
+
+
+          $('.landscape').mouseenter(function() {
+            $('.landscape__image').addClass('showme');
+          });
+
+
+            $('.landscape').mouseleave(function() {
+              $('.landscape__image').removeClass('showme');
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
