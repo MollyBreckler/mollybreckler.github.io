c22d06b7bea9864c9efa0ea6958329f0778f6157
MollyBreckler <lizabreckler@LizaBreklersMBP.gateway>
2017-05-31 18:55:02 +1000

    changes

--- project3/assets/js/main.js
+++ project3/assets/js/main.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
   // Add jQuery here
 
-  $(".oase").mouseenter(function() {
-    $(".oase__image").addClass("showme");
-  });
-
-  $(".oase").mouseleave(function() {
-    $(".oase__image").removeClass("showme");
-  });
 
   $(".magazine").mouseenter(function() {
     $(".magazine__image").addClass("showme");
@@ -16,12 +9,3 @@ $(document).ready(function() {
   $(".magazine").mouseleave(function() {
     $(".magazine__image").removeClass("showme");
   });
-
-  $(".landscape").mouseenter(function() {
-    $(".landscape__image").addClass("showme");
-  });
-
-  $(".landscape").mouseleave(function() {
-    $(".landscape__image").removeClass("showme");
-  });
-});
