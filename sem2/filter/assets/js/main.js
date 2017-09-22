072536afe203b1970fce5256dadb15651b557279
MollyBreckler <mjbre5@student.monash.edu>
2017-09-22 12:02:58 +1000

    archive

---
+++ sem2/filter/assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+$('.nav_ad').click(function(event)){
+  $('.item').addClass('hide');
+
+  $('.item.ad').removeClass('hide')
+  $('.item.ad').addClass('show');
+
+}
+
+
+
+  });
